d2fe2739a660a16a81e64eb39bc7fe646b6f778c
Barbarian0123 <regent.0123@gmail.com>
2017-01-22 17:45:46 +0700

    [Source] SplitCode

---
+++ tool/sourcecode/SplitCode/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SplitCode
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            SplitProject(); 
+        }
+
+        public static void SplitProject()
+        {
+            const string filename = "../servicer/larpmerge.pwn";
+
+            try
+            {
+                var content = File.ReadAllLines(filename).ToList();
+                var arrayIndex =
+                    Enumerable.Range(0, content.Count)
+                        .Where(x => content[x].StartsWith("//") && content[x].EndsWith("TM"))
+                        .ToList();
+               for(var i = 0; i < arrayIndex.Count; i++)
+               {
+                   var fileName = content.ElementAt(arrayIndex[i]).Replace("//", "").Replace(" TM", "");
+                   if (File.Exists(fileName))
+                       File.Delete(fileName);
+                   File.WriteAllLines(fileName,
+                       arrayIndex[i] == arrayIndex.Last()
+                           ? content.GetRange(arrayIndex[i] + 1, content.Count - arrayIndex[i] - 1)
+                           : content.GetRange(arrayIndex[i] + 1, arrayIndex[i + 1] - arrayIndex[i] - 1));
+               }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nError");
+                Console.WriteLine(ex);
+                Console.ReadKey();
+            }
+        }
+    }
+}
